4fee603216489f0a10ee3822482147bb8c933272
Cole Cezeaux <colecezeaux@gmail.com>
2020-08-10 19:30:24 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -20,10 +20,10 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  function sayMy() {
+  console.log(name);
+  var name = function sayMy() {
     var name = "Kristin";
-    console.log(name);
+    
   }
 }
 
